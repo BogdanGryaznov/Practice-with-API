f217e1e111be201b87545c9e937681b000b1784f
ibka2014 <ksas1000watt@vk.com>
2023-03-03 12:01:41 +0300

    Migration v1

---
+++ Third_task/ConsoleApp1/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Third_task.Models
+{
+    public partial class Order
+    {
+        public int IdUser { get; set; }
+        public int OrderId { get; set; }
+        public int DeliveryId { get; set; }
+        public int StatusId { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual User IdUserNavigation { get; set; } = null!;
+    }
+}
