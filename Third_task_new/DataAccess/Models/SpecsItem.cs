757777a20f0cf60822ae5874ba3a2e759a7623af
BogdanGryaznov <ksas1000watt@vk.com>
2023-03-19 20:04:11 +0300

    added new version of 5th work

---
+++ Third_task_new/DataAccess/Models/SpecsItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+public partial class SpecsItem
+{
+    public int SpecItemId { get; set; }
+
+    public int ItemId { get; set; }
+
+    public int SpecId { get; set; }
+
+    public string? SpecValue { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public virtual Product Item { get; set; } = null!;
+
+    public virtual Spec Spec { get; set; } = null!;
+}
