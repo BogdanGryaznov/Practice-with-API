f85d10e3bb2c49ede5c6ea553b51dca672576b62
zabey <ksas1000watt@vk.com>
2023-04-07 09:09:35 +0300

    updt

---
+++ sixth_task/DataAccess/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public bool IsDeleted { get; set; }
+
+    public virtual ICollection<Product> Products { get; } = new List<Product>();
+
+    public virtual ICollection<Spec> Specs { get; } = new List<Spec>();
+}
