599a4e015b67db8bec87040ecc47e88d051e6629
zabey <ksas1000watt@vk.com>
2023-04-07 11:10:50 +0300

    Шестое задание

---
+++ SixthTask/Domain/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public partial class Product
+    {
+        public int ItemId { get; set; }
+
+        public string ItemName { get; set; } = null!;
+
+        public string? ItemDescription { get; set; }
+
+        public int WarehouseQuantity { get; set; }
+
+        public int? Price { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public virtual ICollection<Cart> Carts { get; } = new List<Cart>();
+
+        public virtual Category? Category { get; set; }
+
+        public virtual ICollection<OrdersItem> OrdersItems { get; } = new List<OrdersItem>();
+
+        public virtual ICollection<SpecsItem> SpecsItems { get; } = new List<SpecsItem>();
+    }
+}
+
+
